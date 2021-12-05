159582ca5f8db6d6134095caf218c1bb4292efaa
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-12-05 13:11:21 +0100

    enable force update and improve cleaning

--- pages_to_words.py
+++ pages_to_words.py
@@ -3,6 +3,8 @@ import re
 import json
 import nltk
 from glob import glob
+from tqdm import tqdm
+import argparse
 
 import config
 
@@ -17,23 +19,35 @@ def should_keep(token, remove_words=[]):
 
 
 if __name__ == "__main__":
+    # add argument parser
+    parser = argparse.ArgumentParser(description=f"Convert pages from {config.PATH_CLEAN} to list of processed words ready for text analysis, stored in {config.PATH_WORDS}.")
+    parser.add_argument('-f', dest='force', action='store_true')
+    parser.set_defaults(force=False)
+    args = parser.parse_args()
+    print()
+
     # define tokenizer and lemmatizer
     wpt = nltk.tokenize.WordPunctTokenizer()
     wnl = nltk.stem.wordnet.WordNetLemmatizer()
 
     # define words/tokens to remove
     remove_words = nltk.corpus.stopwords.words('english')
+    remove_words += ['patch', 'player']
 
     # create folder if it doesn't exist
     if not os.path.exists(config.PATH_WORDS):
         os.makedirs(config.PATH_WORDS)
     
     # create word file for each cleaned character page
-    for fpath in glob(config.PATH_CLEAN + '*.txt'):
-        # check if that file is already handled
+    files = glob(config.PATH_CLEAN + '*.txt')
+    characters = [f.split('\\')[-1].replace('.txt', '').replace('_', ' ') for f in files]
+    for fpath in tqdm(files, desc='Convert clean files to list of words'):
         savepath = config.PATH_WORDS + fpath.split('\\')[-1]
-        if os.path.isfile(savepath):
-            continue
+        
+        # check if that file is already handled
+        if not args.force:
+            if os.path.isfile(savepath):
+                continue
 
         # read in cleaned character text file
         with open(fpath, "r", encoding="utf-8") as f:
@@ -41,10 +55,17 @@ if __name__ == "__main__":
 
         # remove stuff regarding patches
         text = re.sub(r'Patch \d.\d.\d \(\d\d\d\d\-\d\d\-\d\d\)\:', ' ', text)
+
+        # remove character names from text
+        for character in characters:
+            text = text.replace(character, ' ')
         
         # butcher contractions (')
-        # important to keep meaning for e.g. "A'dal" and "Sha'tar"
-        text = text.replace("'", "").lower()
+        # important to keep meaning for e.g. "Sha'tar"
+        text = text.replace("'", "")
+        
+        # lower text
+        text = text.lower()
 
         # tokenize text
         tokens = wpt.tokenize(text)
